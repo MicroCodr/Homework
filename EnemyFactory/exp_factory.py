34f7733c98fa3227c6a77178c363b87f02dfcb64
Ayush Singh <ayushcool157@gmail.com>
2025-11-05 21:57:07 -0800

    testing123

--- EnemyFactory/exp_factory.py
+++ EnemyFactory/exp_factory.py
@@ -1,1 +1,20 @@
-testing123
+import random
+
+class Enemy:
+    def __init__(self, name, health, attack):
+        self.name = name
+        self.health = health
+        self.attack = attack
+
+    def __str__(self):
+        return f"{self.name} - Health: {self.health}, Attack: {self.attack}"
+
+
+class ExpGoblin(Enemy):
+    def __init__(self):
+        super().__init__("Expert Goblin, random.randint(30,40), random.randint(10,15)")
+
+
+class ExpTroll(Enemy):
+    def __init__(self):
+        super().__init__("Expert Troll, random.randint(50,70), random.randint(15,25)")
